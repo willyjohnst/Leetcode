31edee84a03ecffb5a80ac177b564e0da16d944f
William Johnston <william.johnstonspc@gmail.com>
2025-09-24 22:03:04 -0400

    Initial commit: 5. Longest Palindromic Substring

--- Python/Longest Substring Without Repeating Characters.py
+++ Python/Longest Substring Without Repeating Characters.py
@@ -14,15 +14,15 @@ Explanation: The answer is "abc", with the length of 3.
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left, right, maxLength = 0, 0, 0
-        curr_string = {};
-        while (right < len(s)):
-            if (s[right] not in curr_string):
-                curr_string.update({s[right]:s[right]})
-                right += 1;
-                maxLength = max(maxLength, right - left)
-            else:
-                curr_string.pop(s[left])
+        curr_set = set();
+        for right in range(len(s)):
+            while s[right] in curr_set:
+                curr_set.remove(s[left])
                 left += 1;
-        return maxLength;
+
+            curr_set.add(s[right])
+            maxLength = max(maxLength, right - left + 1)
+
+        return maxLength
 
 print(Solution.lengthOfLongestSubstring(Solution, "pwwkew"))
