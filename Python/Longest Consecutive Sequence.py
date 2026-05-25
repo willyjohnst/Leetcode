c935d8528ae638ce5691e8d2a39adc3ad76767fe
William Johnston <wjohnst1@friars.providence.edu>
2026-05-24 22:05:05 -0400

    Leetcode Medium Longest Consecutive Sequence

---
+++ Python/Longest Consecutive Sequence.py
@@ -0,0 +1,14 @@
+"""Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time."""
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest = 0
+        num_set = set(nums)
+        for num in num_set:
+            if (num - 1) not in num_set:
+                curr_count = 1
+                while num + curr_count in num_set:
+                    curr_count += 1
+                longest = max(longest, curr_count)
+        return longest
